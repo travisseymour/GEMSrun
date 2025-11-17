04e270761880e68111dc9496cf5da336e7242469
nogard <nogard@ucsc.edu>
2025-11-17 12:12:32 -0800

    fixed missing var in __init__.py

--- gemsrun/__init__.py
+++ gemsrun/__init__.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from loguru import logger as log  # noqa: F401
 from PySide6.QtCore import QSettings
+from PySide6.QtGui import QFont
 from PySide6.QtWidgets import QApplication
 
 CONFIG_PATH: Path | None = None
@@ -12,3 +13,5 @@ SETTINGS: QSettings | None = None
 
 app_short_name = "GEMSrun"
 app_long_name = "GEMS Runner"
+
+default_font: QFont = QFont()
